f9fea5fbad660940a0a2c84365e982098d71a951
Sanket Shahane <shahanesanket24@gmail.com>
2023-06-17 17:37:18 -0700

    third part api implimented

--- flask Server/flaskServer.py
+++ flask Server/flaskServer.py
@@ -7,10 +7,21 @@ Created on Sun Nov 27 19:56:31 2022
 
 from flask import Flask, jsonify, request
 from MovieRecommendation import MovieRecommendation
+import http.client
+import json
 
 app = Flask(__name__)
 
 movie = MovieRecommendation()
+
+
+conn = http.client.HTTPSConnection("moviesdatabase.p.rapidapi.com")
+
+headers = {
+    'X-RapidAPI-Key': "3e1f1dda8cmshe957d0711f80c46p1e0589jsncc633598dc16",
+    'X-RapidAPI-Host': "moviesdatabase.p.rapidapi.com"
+}
+
 
 @app.route('/')
 def start():
@@ -25,10 +36,14 @@ def topMovies():
 @app.route("/title",methods=["POST"])
 def findLikeMovies():
     try:
+        detailsData=[]
         jsonData=request.get_json()
-        print(jsonData)
-        data = movie.get_index_from_title(jsonData)
-        return jsonify(data)
+        data = movie.get_index_from_title(jsonData["title"])
+        for d in data:
+           details = getMovieDetailsFromAPI(d)
+           tp= json.loads(details)
+           detailsData.append(tp)
+        return detailsData
     except:
         return jsonify(["Wrong Title"])
         
@@ -37,11 +52,24 @@ def findLikeMovies():
 @app.route("/genres", methods=["POST"])
 def findMoviesInGenres():
     try:
+        detailsData=[]
         jsonData=request.get_json()
-        data = movie.get_index_from_genre(jsonData)
-        return jsonify(data)
+        data = movie.get_index_from_genre(jsonData["genre"])
+        for d in data:
+           details = getMovieDetailsFromAPI(d)
+           tp= json.loads(details)
+           detailsData.append(tp)
+        return detailsData
     except:
         return jsonify(["Wrong Genres"])
+    
+def getMovieDetailsFromAPI(title):
+    title=title.replace(" ","%20")
+    conn.request("GET", f"/titles/search/title/{title}?exact=true&titleType=movie", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    conn.close()
+    return data.decode()
     
 
 if __name__ == '__main__':
